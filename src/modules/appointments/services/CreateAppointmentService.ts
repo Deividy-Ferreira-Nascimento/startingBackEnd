2c18d4b2e23d6aa17344f481aaa6775562e2b224
deividy <deividyferreira.n@gmail.com>
2021-10-09 18:49:49 -0300

    Reescrevendo Repositorios

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -25,12 +25,12 @@ class CreateAppointmentService {
   }
 
 
-  const appointment = appointmentsRepository.create({
+  const appointment = await appointmentsRepository.create({
     provider_id,
     date:appointmentDate
 
   })
-  await appointmentsRepository.save(appointment)
+
 
   return appointment;
   }
