74a7d989c0fa29dc6984ff02026a49677b3e19c3
deividy <deividyferreira.n@gmail.com>
2021-10-12 22:58:25 -0300

    Atualizacao de avatar

--- src/modules/users/repositories/fakes/FakeUsersRepository.ts
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -1,4 +1,3 @@
-
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
 
@@ -6,8 +5,7 @@ import User from '../../infra/typeorm/entities/User';
 import { uuid } from 'uuidv4';
 
 class UsersRepository implements IUsersRepository {
-
-  private users: User[] = []
+  private users: User[] = [];
 
   public async findById(id: string): Promise<User | undefined> {
     const findId = this.users.find(user => user.id === id);
@@ -16,27 +14,28 @@ class UsersRepository implements IUsersRepository {
   }
 
   public async findByEmail(email: string): Promise<User | undefined> {
-    const findEmail = this.users.find(user => user.email === email)
+    const findEmail = this.users.find(user => user.email === email);
 
     return findEmail;
   }
 
   public async create(userData: ICreateUserDTO): Promise<User> {
-    const user = new User()
+    const user = new User();
 
-    Object.assign(user, { id:uuid() }, userData)
+    Object.assign(user, { id: uuid() }, userData);
 
-    this.users.push(user)
-
-
-    const findIndex = this.users.findIndex(findUser => findUser.id === user.id )
-
-    this.users[findIndex] = user
+    this.users.push(user);
 
     return user;
   }
 
+  public async save(user: User): Promise<User> {
+    const findIndex = this.users.findIndex(findUser => findUser.id === user.id);
 
+    this.users[findIndex] = user;
+
+    return user;
+  }
 }
 
 export default UsersRepository;
---
+++ src/modules/users/services/UpdateUserAvatarService.spec.ts
@@ -0,0 +1,72 @@
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeStorageProvide from '@shared/container/providers/StorageProvider/fakes/FakeStorageProvider'
+import AppError from '@shared/errors/AppError';
+import UpdateUserAvatarService from './UpdateUserAvatarService'
+
+describe('UpdateUserAvatar', () => {
+  it('should be able to update avatar', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeStorageProvider = new FakeStorageProvide()
+
+    const updateAvatar = new UpdateUserAvatarService(fakeUsersRepository,fakeStorageProvider);
+
+    const user = await fakeUsersRepository.create({
+      name:'John Snow',
+      email:'joaodasneves@test.com',
+      password:'123456'
+    })
+
+
+    await updateAvatar.execute({
+      user_id: user.id,
+      avatarFilename: 'avatar.jpg'
+    });
+
+    expect(user.avatar).toBe('avatar.jpg');
+  });
+
+
+  it('should not be able to update avatar with an user not exist', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeStorageProvider = new FakeStorageProvide()
+
+    const updateAvatar = new UpdateUserAvatarService(fakeUsersRepository,fakeStorageProvider);
+
+
+    expect(updateAvatar.execute({
+      user_id: 'user-non-exist',
+      avatarFilename: 'avatar.jpg'
+    })).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should delete old avatar when updating new one', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeStorageProvider = new FakeStorageProvide()
+
+    const deleteFile = jest.spyOn(fakeStorageProvider, 'deleteFile')
+
+    const updateAvatar = new UpdateUserAvatarService(fakeUsersRepository,fakeStorageProvider);
+
+    const user = await fakeUsersRepository.create({
+      name:'John Snow',
+      email:'joaodasneves@test.com',
+      password:'123456'
+    })
+
+
+    await updateAvatar.execute({
+      user_id: user.id,
+      avatarFilename: 'avatar.jpg'
+    });
+
+    await updateAvatar.execute({
+      user_id: user.id,
+      avatarFilename: 'avatar2.jpg'
+    });
+
+    expect(deleteFile).toHaveBeenCalledWith('avatar.jpg')
+    expect(user.avatar).toBe('avatar2.jpg');
+  });
+
+
+});
