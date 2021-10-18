f8164ab0304d28c2ca21a258377fc10936552216
deividy <deividyferreira.n@gmail.com>
2021-10-18 18:56:40 -0300

    Template de emails

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -5,6 +5,7 @@ import AppError from '@shared/errors/AppError';
 import IUsersRepository from '../repositories/IUsersRepository';
 import IUserTokensRepository from '../repositories/IUserTokensRepository';
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import { from } from 'form-data';
 
 interface IRequest {
   email: string;
@@ -24,23 +25,28 @@ class SendForgotPasswordEmailService {
   ) {}
 
   public async execute({ email }: IRequest): Promise<void> {
-
     const user = await this.usersRepository.findByEmail(email);
 
-    if(!user) {
-      throw new AppError('User does not exists')
+    if (!user) {
+      throw new AppError('User does not exists');
     }
 
-    const { token } = await this.userTokensRepository.generate(user.id)
+    const { token } = await this.userTokensRepository.generate(user.id);
 
-    await this.mailProvider.sendMail(
-      email,
-      `Pedido de recuperação de senha recebido
-
-      Código de validação para recuperação de conta:
-
-      ${token}`,
-    );
+    await this.mailProvider.sendMail({
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: '[Gobarber] Recuperação de senha',
+      templateData: {
+        template: 'Olá, {{name}}: {{token}}',
+        variables: {
+          name: user.name,
+          token,
+        },
+      },
+    });
   }
 }
 
