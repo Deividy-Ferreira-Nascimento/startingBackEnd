29dd925aeb6d2fa8786647b826497013cb074a4b
deividy <deividyferreira.n@gmail.com>
2021-10-27 15:27:45 -0300

    Cache lista de providers

--- src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
+++ src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
@@ -9,13 +9,19 @@ export default class RedisCacheProvider implements ICashProvider {
     this.client = new Redis(cache.config.redis)
   }
 
-  public async save(key: string, value: string): Promise<void> {
-    await this.client.set(key, value)
+  public async save(key: string, value: any): Promise<void> {
+    await this.client.set(key, JSON.stringify(value))
   };
-  public async recover(key: string):Promise<string | null> {
-    const data = await this.client.get(key)
+  public async recover<T>(key: string):Promise<T | null> {
+    const data = await this.client.get(key);
 
-    return data
+    if(!data) {
+      return null
+    }
+
+    const parsedData = JSON.parse(data) as T
+
+    return parsedData
   }
   public async invalidate(key: string): Promise<void> {};
 }
